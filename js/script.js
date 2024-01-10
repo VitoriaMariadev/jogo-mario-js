2d29f1eb3796801fc8078bc83b8c75becab9c3a1
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2024-01-09 21:26:47 -0300

    FEAT/ Score

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const text = document.querySelector('.score')
+
+let score = 0;
+let isPipePassed = false; // Adicionada variável para controlar se o pipe já foi passado
 
 const jump = () => {
     mario.classList.add('jump');
 
-    setTimeout(() =>{
+    setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
 }
@@ -14,7 +18,8 @@ const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        // Mario atingiu o pipe
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -24,12 +29,19 @@ const loop = setInterval(() => {
 
         mario.src = './Images/game-over.png';
         mario.style.width = '75px';
-        mario.style.marginLeft = '50px'
+        mario.style.marginLeft = '50px';
 
-        clearInterval(loop)
-
+        clearInterval(loop);
+    } else if (pipePosition <= 0 && !isPipePassed) {
+        // Mario passou pelo pipe com sucesso
+        score++;
+        isPipePassed = true; // Marcar o pipe como passado para evitar incremento múltiplo
+        console.log('Score: ', score);
+        text.textContent = `Pontuação: ${score}`;
+    } else if (pipePosition > 0) {
+        isPipePassed = false; // Resetar a marcação quando o pipe sair da tela
     }
 
-}, 10)
+}, 10);
 
 document.addEventListener('keydown', jump);
